623cd6d1de4a400f25545c9619a2e1578a0b88ae
Claudio Mendonça <68253329+ClaudioMendonca-Eng@users.noreply.github.com>
2024-06-25 21:09:36 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,7 @@ function translatorPage() {
 		textProjects.innerHTML = '<span>projetos</span>Confira alguns dos meus projetos recentes.';
 		textProject01.innerHTML = '<h3>Decodificador de texto</h3><span>Aplicativo decodificador de textos.</span>';
 		textProject02.innerHTML = '<h3>Guessing Game</h3><span>Secret number game.</span>';
-		textProject03.innerHTML = '<h3>Santander Dev Week 2024</h3><span>Um Chat Inteligente com os Campeões de LOL usando APIs, Java, Spring Boot e IA.</span>';
+		textProject03.innerHTML = '<h3>Santander Dev Week 2024</h3><span>Um Chat Inteligente com os Campeões de League of Legends usando APIs, Java, Spring Boot e IA.</span>';
 		textTestimonials.innerHTML = 'Depoimentos';
 		textArticle01.innerHTML = '<article>O Cláudio é um excelente profissional, tendo executado uma performance bastante elogiada, durante o tempo em que eu trabalhava na Parex , e pude acompanhá-lo no seu dia dia . Baseado nesse tempo é com satisfação que eu faço essa recomendação.</article><section><span class="cm-name">Paulo Cesar</span><span class="cm-post">SÓCIO-GERENTE - PVP COMERCIO E REPRESENTAÇÕES EIRELI</span></section>';
 		textArticle02.innerHTML = '<article>Aguardando Mais.</span>';
@@ -35,17 +35,17 @@ function translatorPage() {
 		textTranslator.innerHTML = '&lt;HTML Lang="PT-BR"&gt;';
 		textBanner.innerHTML = 'I’m a soft­ware devel­op­er, turning your brainstorming into reality.';
 		textPhraseResume.innerHTML = 'Dedicated to improving your business operations';
-		textResume.innerHTML = 'Turning your brainstorming into reality, my commitment is to apply expertise to create a superior-quality software, boosting the performance and efficiency of your business. Specialized in find out the needs of both small and large enterprises, I am here to bring innovative ideas to achieve and improve  the technological standard of your company.';
+		textResume.innerHTML = 'Turning your brainstorming into reality, my commitment is to apply expertise to create a superior-quality software, boosting the performance and efficiency of your business. Specialized in find out the needs of both small and large enterprises, I am here to bring innovative ideas to improve the technological standards of your company.';
 		textDownload.innerHTML = 'Download Resume  <i class="ri-download-line"></i>';
-		textProjects.innerHTML = '<span>projects</span>Check out a few of my recent project.';
+		textProjects.innerHTML = '<span>projects</span>Check out a few of my recent projects.';
 		textProject01.innerHTML = '<h3Text decoder</h3><span>Text decoder application.</span>';
 		textProject02.innerHTML = '<h3>Guessing Game</h3><span>Secret number game.</span>';
-		textProject03.innerHTML = '<h3>Santander Dev Week 2024</h3><span>A Smart Chat with LOL Champions using APIs, Java, Spring Boot, and AI</span>';
+		textProject03.innerHTML = '<h3>Santander Dev Week 2024</h3><span>A Smart Chat with League of Legends Champions using APIs, Java, Spring Boot, and AI</span>';
 		textTestimonials.innerHTML = 'Testimonials';
 		textArticle01.innerHTML = '<article>Claudio is an excellent professional, having delivered a highly praised performance during the time I worked at Parex, and I had the opportunity to observe his day-to-day activities. Based on that experience, it is with pleasure that I provide this recommendation.</article><section><span class="cm-name">Paulo Cesar</span><span class="cm-post">MANAGING PARTNER - PVP COMERCIO E REPRESENTAÇÕES EIRELI</span></section>';
 		textArticle02.innerHTML = '<article>Waiting for More.</article><section><span class="cm-name">Nome Sobrenome</span><span class="cm-post">Função - Empresa</span></section>';
-		textVacant.innerHTML = 'I am available for working';
-		textFooter.innerHTML = '<span class="cm-copyright">© 2024 All right reserved. <br><a href="https://www.claudiomendonca.eng.br/" target="_blanck">Created by Claudio Mendonça.</span>';
+		textVacant.innerHTML = 'I am available to work';
+		textFooter.innerHTML = '<span class="cm-copyright">© 2024 All rights reserved. <br><a href="https://www.claudiomendonca.eng.br/" target="_blanck">Created by Claudio Mendonça.</span>';
 	}
 
 }
